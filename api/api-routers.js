e3ba04ed6f095354dbb7ac2aa7ea56538f86a7f7
Th5tch3r <amber.phan27@gmail.com>
2020-02-12 22:11:29 -0500

    done with /api/register

--- api/api-routers.js
+++ api/api-routers.js
@@ -1,21 +1,39 @@
-// const express = require('express');
-
-// const router = express.Router();
 
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const authorize = require('../auth/auth-required-middleware.js');
 
-const authRouter = require('../auth/auth-router.js');
+const Users = require('../users/users-model.js');
+
 const usersRouter = require('../users/users-router.js');
 
-// /api/auth
-router.use('/auth', authRouter);
-
 // /api/users
-router.use('/users', usersRouter);
+// router.use('/users', usersRouter);
 
 // /api
 router.get('/', (req, res) => {
     res.status(200).json({api: "Thatcher's api is ready to go!"})
+})
+
+// /api/register
+router.post('/register', (req, res) => {
+    let user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    Users.add(user)
+        .then(newUser => {
+            res.status(201).json(newUser)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({message: "Cannot save this user"})
+        })
+})
+
+router.get('/login', (req, res) => {
+
 })
 
 module.exports = router;
---
+++ auth/auth-required-middleware.js
@@ -0,0 +1,24 @@
+const users = require('../users/users-model.js');
+const bcrypt = require('bcryptjs');
+
+module.exports = (req, res, next) => {
+    const {username, password} = req.headers;
+
+    if(!(username && password)) {
+        res.status(401).json({message: "Invalid credentials"})
+    } else {
+        users.findBy({username})
+            .first()
+            .then(user => {
+                if (user && bcrypt.compareSync(password, user.password)){
+                    next()
+                } else {
+                    res.status(401).json({message: "Invalid crdentials"})
+                }
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json({message: "Server error"})
+            })
+    }
+}
